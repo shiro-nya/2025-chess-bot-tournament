ec3d934b62125a8c719f332146afbd448b00ce0f
shiro__nya <shiro.nya.business@gmail.com>
2025-09-17 00:12:18 -0400

    Py: Complete rewrite of bindings
    Removed cffi dependency, must build libchess.so to use

--- src/py/pychessapi.py
+++ src/py/pychessapi.py
@@ -1,11 +1,20 @@
 from __future__ import annotations
 
-from _chessapi_cffi import ffi, lib
+from ctypes import cdll, CDLL, c_uint8, c_uint64, c_int, c_bool, Structure, c_long, cast, pointer, byref, POINTER, ARRAY
 from enum import Enum
-from typing import Any
+from typing import Any, Union
+import os
+
+
+# import library
+local_dir = os.path.dirname(os.path.realpath(__file__))
+cdll.LoadLibrary(local_dir + "/libchess.so")
+lib = CDLL(local_dir + "/libchess.so")
 
 
 BitBoard = int
+_BitBoard = c_uint64
+_C_enum = c_int
 
 
 class PlayerColor(Enum):
@@ -26,107 +35,91 @@ class GameState(Enum):
     GAME_CHECKMATE = -1,
     GAME_NORMAL = 0,
     GAME_STALEMATE = 1
+    
 
+class Move(Structure):
 
-class Move:
+    _fields_ = [("from", _BitBoard),
+                ("to", _BitBoard),
+                ("promotion", c_uint8),
+                ("capture", c_bool),
+                ("castle", c_bool)]
 
     origin: BitBoard
     target: BitBoard
     promotion: PieceType
     castle: bool
     capture: bool
-    _share_ptr: Any
 
-    def __init__(self, make_ptr: bool = True):
-        if make_ptr:
-            self._share_ptr = ffi.new("Move *")
-
-    @classmethod
-    def load(cls, ptr) -> Move:
-        move = Move()
-        move.origin = getattr(ptr, "from")
-        move.target = getattr(ptr, "to")
-        move.promotion = getattr(ptr, "promotion")
-        move.castle = getattr(ptr, "castle")
-        move.capture = getattr(ptr, "capture")
-        return move
-
-    def dump(self):
-        setattr(self._share_ptr, "from", self.origin)
-        setattr(self._share_ptr, "to", self.target)
-        setattr(self._share_ptr, "promotion", self.promotion)
-        setattr(self._share_ptr, "castle", self.castle)
-        setattr(self._share_ptr, "capture", self.capture)
-        return self._share_ptr[0]
+    def __init__(self):
+        pass
 
 
-class Board:
+class Board(Structure):
 
-    ptr: int
+    _fields_ = []
 
-    def __init__(self, ptr: int):
-        self.ptr = ptr
+    def __init__(self):
+        pass
 
     def __del__(self):
-        lib.chess_free_board(self.ptr)
+        lib.chess_free_board(byref(self))
 
     def get_legal_moves(self) -> list[Move]:
-        moves_len_C = ffi.new("int *")
-        moves_C = lib.chess_get_legal_moves(self.ptr, moves_len_C)
-        moves_len = moves_len_C[0]
-        moves = ffi.unpack(moves_C, moves_len)
-        for i in range(len(moves)):
-            moves[i] = Move.load(moves[i])
+        moves_len_C = c_int(0)
+        moves_C = cast(lib.chess_get_legal_moves(byref(self), byref(moves_len_C)), POINTER(Move))
+        moves_len = moves_len_C.value
+        moves = [moves_C[i] for i in range(moves_len)]
         lib.chess_free_moves_array(moves_C)
         return moves
 
     def is_white_turn(self) -> bool:
-        return lib.chess_is_white_turn(self.ptr)
+        return lib.chess_is_white_turn(byref(self)).value
 
     def skip_turn(self) -> None:
-        lib.chess_skip_turn(self.ptr)
+        lib.chess_skip_turn(byref(self))
 
     def in_check(self) -> bool:
-        return lib.chess_in_check(self.ptr)
+        return lib.chess_in_check(byref(self)).value
 
     def in_checkmate(self) -> bool:
-        return lib.chess_in_checkmate(self.ptr)
+        return lib.chess_in_checkmate(byref(self)).value
 
     def in_draw(self) -> bool:
-        return lib.chess_in_draw(self.ptr)
+        return lib.chess_in_draw(byref(self)).value
 
     def can_castle_kingside(self, color: PlayerColor) -> bool:
-        return lib.chess_can_castle_kingside(self.ptr, color)
+        return lib.chess_can_castle_kingside(byref(self), color).value
 
     def can_castle_queenside(self, color: PlayerColor) -> bool:
-        return lib.chess_can_castle_queenside(self.ptr, color)
+        return lib.chess_can_castle_queenside(byref(self), color).value
 
     def is_game_ended(self) -> GameState:
-        return lib.chess_is_game_ended(self.ptr)
+        return GameState(lib.chess_is_game_ended(byref(self)).value)
 
     def zobrist_key(self) -> int:
-        return lib.chess_zobrist_key(self.ptr)
+        return lib.chess_zobrist_key(byref(self)).value
 
     def make_move(self, move: Move) -> None:
-        lib.chess_make_move(self.ptr, move.dump())
+        lib.chess_make_move(byref(self), move)
 
     def undo_move(self) -> None:
-        lib.chess_undo_move(self.ptr)
+        lib.chess_undo_move(byref(self))
 
     def get_bitboard(self, color: PlayerColor, piece_type: PieceType) -> BitBoard:
-        return lib.chess_get_bitboard(self.ptr, color, piece_type)
+        return lib.chess_get_bitboard(byref(self), color, piece_type).value
     
     def get_piece_from_index(self, index: int) -> PieceType:
-        return lib.chess_get_piece_from_index(self.ptr, index)
+        return PieceType(lib.chess_get_piece_from_index(byref(self), index).value)
 
     def get_piece_from_bitboard(self, bitboard: BitBoard) -> PieceType:
-        return lib.chess_get_piece_from_bitboard(self.ptr, bitboard)
+        return PieceType(lib.chess_get_piece_from_bitboard(byref(self), bitboard).value)
 
     def get_color_from_index(self, index: int) -> PlayerColor:
-        return lib.chess_get_color_from_index(self.ptr, index)
+        return PlayerColor(lib.chess_get_color_from_index(byref(self), index).value)
 
     def get_color_from_bitboard(self, bitboard: BitBoard) -> PieceType:
-        return lib.chess_get_color_from_bitboard(self.ptr, bitboard)
+        return PieceType(lib.chess_get_color_from_bitboard(byref(self), bitboard).value)
 
 
 class API:
@@ -137,11 +130,11 @@ class API:
 
     @classmethod
     def get_board(cls) -> Board:
-        return Board(lib.chess_get_board())
+        return lib.chess_get_board().contents
     
     @classmethod
     def push(cls, move: Move) -> None:
-        lib.chess_push(move.dump())
+        lib.chess_push(move)
 
     @classmethod
     def done(cls) -> None:
@@ -149,20 +142,77 @@ class API:
 
     @classmethod
     def get_time_millis(cls) -> int:
-        return lib.chess_get_time_millis()
+        return lib.chess_get_time_millis().value
 
     @classmethod
     def get_opponent_time_millis(cls) -> int:
-        return lib.chess_get_opponent_time_millis()
+        return lib.chess_get_opponent_time_millis().value
 
     @classmethod
     def get_elapsed_time_millis(cls) -> int:
-        return lib.chess_get_elapsed_time_millis()
+        return lib.chess_get_elapsed_time_millis().value
     
     @classmethod
     def get_index_from_bitboard(cls, bitboard: BitBoard) -> int:
-        return lib.chess_get_index_from_bitboard(bitboard)
+        return lib.chess_get_index_from_bitboard(bitboard).value
     
     @classmethod
     def get_bitboard_from_index(cls, index: int) -> BitBoard:
-        return lib.chess_get_bitboard_from_index(index)
+        return lib.chess_get_bitboard_from_index(index).value
+
+
+# set type restrictions
+lib.chess_get_board.argtypes = ()
+lib.chess_get_board.restype = POINTER(Board)
+lib.chess_get_legal_moves.argtypes = (POINTER(Board), POINTER(c_int))
+lib.chess_get_legal_moves.restype = POINTER(Move)
+lib.chess_is_white_turn.argtypes = (POINTER(Board),)
+lib.chess_is_white_turn.restype = c_bool
+lib.chess_skip_turn.argtypes = (POINTER(Board),)
+lib.chess_skip_turn.restype = None
+lib.chess_in_check.argtypes = (POINTER(Board),)
+lib.chess_in_check.restype = c_bool
+lib.chess_in_checkmate.argtypes = (POINTER(Board),)
+lib.chess_in_checkmate.restype = c_bool
+lib.chess_in_draw.argtypes = (POINTER(Board),)
+lib.chess_in_draw.restype = c_bool
+lib.chess_can_kingside_castle.argtypes = (POINTER(Board), _C_enum)
+lib.chess_can_kingside_castle.restype = c_bool
+lib.chess_can_queenside_castle.argtypes = (POINTER(Board), _C_enum)
+lib.chess_can_queenside_castle.restype = c_bool
+lib.chess_is_game_ended.argtypes = (POINTER(Board),)
+lib.chess_is_game_ended.restype = c_bool
+lib.chess_zobrist_key.argtypes = (POINTER(Board),)
+lib.chess_zobrist_key.restype = c_uint64
+lib.chess_make_move.argtypes = (POINTER(Board), Move)
+lib.chess_make_move.restype = None
+lib.chess_undo_move.argtypes = (POINTER(Board),)
+lib.chess_undo_move.restype = None
+lib.chess_free_board.argtypes = (POINTER(Board),)
+lib.chess_free_board.restype = None
+lib.chess_get_bitboard.argtypes = (POINTER(Board), _C_enum, _C_enum)
+lib.chess_get_bitboard.restype = _BitBoard
+lib.chess_push.argtypes = (Move,)
+lib.chess_push.restype = None
+lib.chess_done.argtypes = ()
+lib.chess_done.restype = None
+lib.chess_get_time_millis.argtypes = ()
+lib.chess_get_time_millis.restype = c_long
+lib.chess_get_opponent_time_millis.argtypes = ()
+lib.chess_get_opponent_time_millis.restype = c_long
+lib.chess_get_elapsed_time_millis.argtypes = ()
+lib.chess_get_elapsed_time_millis.restype = c_long
+lib.chess_free_moves_array.argtypes = (POINTER(Move),)
+lib.chess_free_moves_array.restype = None
+lib.chess_get_piece_from_index.argtypes = (POINTER(Board), c_int)
+lib.chess_get_piece_from_index.restype = _C_enum
+lib.chess_get_piece_from_bitboard.argtypes = (POINTER(Board), _BitBoard)
+lib.chess_get_piece_from_bitboard.restype = _C_enum
+lib.chess_get_color_from_index.argtypes = (POINTER(Board), c_int)
+lib.chess_get_color_from_index.restype = _C_enum
+lib.chess_get_color_from_bitboard.argtypes = (POINTER(Board), _BitBoard)
+lib.chess_get_color_from_bitboard.restype = _C_enum
+lib.chess_get_index_from_bitboard.argtypes = (_BitBoard,)
+lib.chess_get_index_from_bitboard.restype = c_int
+lib.chess_get_bitboard_from_index.argtypes = (c_int,)
+lib.chess_get_bitboard_from_index.restype = _BitBoard
